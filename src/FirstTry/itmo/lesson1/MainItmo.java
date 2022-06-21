9a615c6f0e172d0837692f32136d32b7dd0549bf
AntonLachin <lachin-1993@mail.ru>
2022-06-21 19:09:58 +0300

    Initial commit

---
+++ src/FirstTry/itmo/lesson1/MainItmo.java
@@ -0,0 +1,69 @@
+package FirstTry.itmo.lesson1;
+
+public class MainItmo {
+    public static void main (String[] args){
+//        System.out.println("Hello world!");
+        byte b = 115;
+        short sh = 123;
+        int i = 300;
+        long l = 1000L;
+        float f = 12.3f;
+        double d = 14.5d;
+
+        char c = 'a';
+        boolean isTrue = true;
+
+        byte r =(byte) (b + i);
+
+//        System.out.println(r);
+
+        long q = b + i;
+
+        double d1 =(double) l/i;
+
+//        System.out.println(d1);d
+
+        String string = "Java";
+        String str = new String("Java");
+//        System.out.println(string);
+
+//        if (true) {
+////            logic
+//        }
+//
+//        if (true) {
+////            logic
+//        } else{
+//            //            logic
+//        }
+//
+//        if (true) {
+////            logic
+//        } else if (false){
+//            //            logic
+//        }else if (false){
+//            //            logic
+//        }else if (false){
+//            //            logic
+//        } else {
+//            //            logic
+//        }
+
+        int x = 10;
+        int y = 20;
+
+        // x == y; x != y; x < y; x >= y; x % 2 ==0;
+
+        if (x == y && x > y) {
+            //            logic
+        }
+
+        if (x == y || x > y) {
+            //            logic
+        }
+        int a1 = 0;
+
+//        String s1 = a1 == 2 ? "Hello" : "Goodbye";
+//        System.out.println(s1);
+    }
+}
